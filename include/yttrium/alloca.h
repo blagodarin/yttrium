f338e6b28f9015fdd3c9ee3b511b265c74edfe31
Sergey Blagodarin <s.blagodarin@gmail.com>
2015-09-05 15:28:22 +0300

    Comments.

--- include/yttrium/alloca.h
+++ include/yttrium/alloca.h
@@ -1,19 +1,21 @@
 /// \file
-/// \brief
+/// \brief Stack memory allocation.
 
 #ifndef __Y_ALLOCA_H
 #define __Y_ALLOCA_H
 
 #include <yttrium/global.h>
 
-///
+/// \def Y_ALLOCA
+/// \brief Allocates memory on the stack.
+/// \return Pointer to the allocated memory.
 #if Y_COMPILER_GCC
 	#define Y_ALLOCA(size) __builtin_alloca(size)
 #elif Y_COMPILER_MSVC
 	#include <malloc.h>
 	#define Y_ALLOCA(size) _alloca(size)
 #else
-	#error
+	#error Y_ALLOCA is unavailable for your compiler.
 #endif
 
-#endif // __Y_ALLOCA_H
+#endif
