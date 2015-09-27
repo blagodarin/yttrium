83469eb5734e2fbefda5a77fd706d2ff96c5b916
Sergey Blagodarin <s.blagodarin@gmail.com>
2015-09-27 12:24:09 +0300

    FOURCC literals fix.

--- src/base/fourcc.h
+++ src/base/fourcc.h
@@ -8,26 +8,28 @@ namespace Yttrium
 {
 	constexpr uint32_t operator"" _fourcc(const char* text, size_t)
 	{
-		return static_cast<uint32_t>(text[0])
-			| static_cast<uint32_t>(text[1]) << 8
-			| static_cast<uint32_t>(text[2]) << 16
-			| static_cast<uint32_t>(text[3]) << 24;
+		return uint32_t{static_cast<unsigned char>(text[0])}
+			| uint32_t{static_cast<unsigned char>(text[1])} << 8
+			| uint32_t{static_cast<unsigned char>(text[2])} << 8 * 2
+			| uint32_t{static_cast<unsigned char>(text[3])} << 8 * 3
+			;
 	}
 
 	constexpr uint64_t operator"" _eightcc(const char* text, size_t)
 	{
-		return static_cast<uint64_t>(text[0])
-			| static_cast<uint64_t>(text[1]) << 8
-			| static_cast<uint64_t>(text[2]) << 16
-			| static_cast<uint64_t>(text[3]) << 24
-			| static_cast<uint64_t>(text[4]) << 32
-			| static_cast<uint64_t>(text[5]) << 40
-			| static_cast<uint64_t>(text[6]) << 48
-			| static_cast<uint64_t>(text[7]) << 56;
+		return uint64_t{static_cast<unsigned char>(text[0])}
+			| uint64_t{static_cast<unsigned char>(text[1])} << 8
+			| uint64_t{static_cast<unsigned char>(text[2])} << 8 * 2
+			| uint64_t{static_cast<unsigned char>(text[3])} << 8 * 3
+			| uint64_t{static_cast<unsigned char>(text[4])} << 8 * 4
+			| uint64_t{static_cast<unsigned char>(text[5])} << 8 * 5
+			| uint64_t{static_cast<unsigned char>(text[6])} << 8 * 6
+			| uint64_t{static_cast<unsigned char>(text[7])} << 8 * 7
+			;
 	}
 
-	static_assert("ABCD"_fourcc == 0x44434241, "FOURCC compile time test failed");
-	static_assert("ABCDEFGH"_eightcc == 0x4847464544434241, "EIGHTCC compile time test failed");
+	static_assert("\x00\x80\x7f\xff"_fourcc == 0xff7f8000, "FOURCC compile time test failed");
+	static_assert("\x00\x01\x7e\x7f\x80\x81\xfe\xff"_eightcc == 0xfffe81807f7e0100, "EIGHTCC compile time test failed");
 }
 
 #endif
