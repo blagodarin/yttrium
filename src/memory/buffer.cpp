1d106c6479030dc6ed4c1b2dcf58d3a1e79a8df2
Sergey Blagodarin <s.blagodarin@gmail.com>
2016-01-03 14:17:11 +0300

    Wasted buffer memory tracking.

--- src/memory/buffer.cpp
+++ src/memory/buffer.cpp
@@ -10,35 +10,44 @@
 	#include <iostream>
 #endif
 
+#define TRACK_WASTED_MEMORY Y_IS_DEBUG
+
 namespace Yttrium
 {
 	namespace
 	{
-		struct BufferMemoryStatus
+		struct AtomicPair
 		{
-			std::atomic<size_t> _allocated{0};
-			std::atomic<size_t> _max_allocated{0};
+			std::atomic<size_t> _value{0};
+			std::atomic<size_t> _max_value{0};
 
-			void on_allocated(size_t size)
+			void add(size_t difference)
 			{
-				auto old_allocated = _allocated.fetch_add(size);
-				const auto new_allocated = old_allocated + size;
-				while (!_max_allocated.compare_exchange_strong(old_allocated, new_allocated))
-					if (old_allocated >= new_allocated)
+				auto old_value = _value.fetch_add(difference);
+				const auto new_value = old_value + difference;
+				while (!_max_value.compare_exchange_strong(old_value, new_value))
+					if (old_value >= new_value)
 						break;
 			}
+		};
 
-			void on_deallocated(size_t size)
-			{
-				_allocated.fetch_sub(size);
-			}
+		struct BufferMemoryStatus
+		{
+			AtomicPair _allocated;
+		#if TRACK_WASTED_MEMORY
+			AtomicPair _wasted;
+		#endif
 
 		#if Y_IS_DEBUG
 			~BufferMemoryStatus()
 			{
-				const auto max_allocated = _max_allocated.load();
+				const auto max_allocated = _allocated._max_value.load();
 				std::cerr << "(DEBUG) Max buffer memory allocated: " << max_allocated / 1024 << " KiB" << std::endl;
-				const auto allocated = _allocated.load();
+			#if TRACK_WASTED_MEMORY
+				const auto max_wasted = _wasted._max_value.load();
+				std::cerr << "(DEBUG) Max buffer memory wasted: " << max_wasted << " B" << std::endl;
+			#endif
+				const auto allocated = _allocated._value.load();
 				if (allocated > 0)
 					std::cerr << "(ERROR) Buffer memory leaked: " << allocated / 1024 << " KiB" << std::endl;
 			}
@@ -55,7 +64,11 @@ namespace Yttrium
 	{
 		if (!_data)
 			throw std::bad_alloc();
-		_buffer_memory_status.on_allocated(_capacity);
+		_buffer_memory_status._allocated.add(_capacity);
+	#if TRACK_WASTED_MEMORY
+		if (_capacity > _size)
+			_buffer_memory_status._wasted.add(_capacity - _size);
+	#endif
 	}
 
 	void Buffer::reset(size_t size)
@@ -66,16 +79,28 @@ namespace Yttrium
 			const auto new_data = _data ? pages_reallocate(_data, _capacity, new_capacity) : pages_allocate(new_capacity);
 			if (!new_data)
 				throw std::bad_alloc();
-			_buffer_memory_status.on_allocated(new_capacity - _capacity);
+		#if TRACK_WASTED_MEMORY
+			_buffer_memory_status._wasted._value.fetch_sub(_capacity - _size);
+		#endif
+			_buffer_memory_status._allocated.add(new_capacity - _capacity);
+		#if TRACK_WASTED_MEMORY
+			_buffer_memory_status._wasted.add(new_capacity - size);
+		#endif
 			_capacity = new_capacity;
 			_data = new_data;
 		}
+	#if TRACK_WASTED_MEMORY
+		else if (size < _size)
+			_buffer_memory_status._wasted.add(_size - size);
+		else
+			_buffer_memory_status._wasted._value.fetch_sub(size - _size);
+	#endif
 		_size = size;
 	}
 
 	size_t Buffer::max_memory_allocated() noexcept
 	{
-		return _buffer_memory_status._max_allocated;
+		return _buffer_memory_status._allocated._max_value;
 	}
 
 	size_t Buffer::memory_granularity() noexcept
@@ -96,7 +121,10 @@ namespace Yttrium
 		if (_data)
 		{
 			pages_deallocate(_data, _capacity);
-			_buffer_memory_status.on_deallocated(_capacity);
+		#if TRACK_WASTED_MEMORY
+			_buffer_memory_status._wasted._value.fetch_sub(_capacity - _size);
+		#endif
+			_buffer_memory_status._allocated._value.fetch_sub(_capacity);
 		}
 	}
 
@@ -105,7 +133,10 @@ namespace Yttrium
 		if (_data)
 		{
 			pages_deallocate(_data, _capacity);
-			_buffer_memory_status.on_deallocated(_capacity);
+		#if TRACK_WASTED_MEMORY
+			_buffer_memory_status._wasted._value.fetch_sub(_capacity - _size);
+		#endif
+			_buffer_memory_status._allocated._value.fetch_sub(_capacity);
 		}
 		_size = other._size;
 		_capacity = other._capacity;
