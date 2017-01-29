cf2c88e7a92e5d4604c4fc5552af0bc4b81b9e00
Sergey Blagodarin <s.blagodarin@gmail.com>
2017-01-29 17:25:22 +0300

    Minor improvements.

--- ywinmain/ywinmain.cpp
+++ ywinmain/ywinmain.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <windows.h>
 
 int main(int, char**);
@@ -6,18 +8,17 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	int argc = 0;
 	const auto argv_w = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
-	const auto argv = new char*[argc + 1];
+	std::vector<char*> argv;
 	for (int i = 0; i < argc; ++i)
 	{
 		const auto buffer_size = ::WideCharToMultiByte(CP_ACP, 0, argv_w[i], -1, nullptr, 0, nullptr, nullptr);
-		argv[i] = new char[buffer_size];
+		argv.emplace_back(new char[buffer_size]);
 		::WideCharToMultiByte(CP_ACP, 0, argv_w[i], -1, argv[i], buffer_size, nullptr, nullptr);
 	}
-	argv[argc] = nullptr;
+	argv.emplace_back(nullptr);
 	::LocalFree(argv_w);
-	const auto result = main(argc, argv);
-	for (int i = 0; i < argc; ++i)
-		delete[] argv[i];
-	delete[] argv;
+	const auto result = main(argc, argv.data());
+	for (const auto arg : argv)
+		delete[] arg;
 	return result;
 }
