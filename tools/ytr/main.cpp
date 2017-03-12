39ecb5deb563828d7b86f1e8aa7964f567d7bda3
Sergey Blagodarin <s.blagodarin@gmail.com>
2017-03-12 13:59:48 +0300

    ytr switched to IonReader.

--- tools/ytr/main.cpp
+++ tools/ytr/main.cpp
@@ -1,56 +1,10 @@
-#include <yttrium/ion/document.h>
-#include <yttrium/ion/node.h>
-#include <yttrium/ion/object.h>
-#include <yttrium/ion/value.h>
+#include <yttrium/ion/reader.h>
 #include <yttrium/storage/reader.h>
 #include <yttrium/translation.h>
 
 #include <iostream>
 
 using namespace Yttrium;
-
-bool update_tr(Translation& translation, const IonObject& tr_object)
-{
-	if (tr_object.size() != 1)
-		return false;
-
-	const auto& tr_node = *tr_object.begin();
-	if (tr_node.name() != "tr" || tr_node.size() != 1)
-		return false;
-
-	const auto& tr_value = *tr_node.first();
-	if (tr_value.type() != IonValue::Type::String)
-		return false;
-
-	translation.add(tr_value.string());
-	return true;
-}
-
-void update_translation(Translation& translation, const IonObject& source);
-
-void update_translation(Translation& translation, const IonValue& source)
-{
-	switch (source.type())
-	{
-	case IonValue::Type::List:
-		for (const auto& value : source.list())
-			update_translation(translation, value);
-		break;
-	case IonValue::Type::Object:
-		if (!update_tr(translation, *source.object()))
-			update_translation(translation, *source.object());
-		break;
-	default:
-		break;
-	}
-}
-
-void update_translation(Translation& translation, const IonObject& source)
-{
-	for (const auto& node : source)
-		for (const auto& value : node)
-			update_translation(translation, value);
-}
 
 int main(int argc, char** argv)
 {
@@ -60,7 +14,7 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	const auto translation = Translation::open(Reader(argv[1]));
+	const auto translation = Translation::open(Reader{argv[1]});
 	if (!translation)
 	{
 		std::cerr << "ERROR: Unable to open translation \"" << argv[1] << "\"\n";
@@ -69,13 +23,39 @@ int main(int argc, char** argv)
 
 	for (int i = 2; i < argc; ++i)
 	{
-		const auto document = IonDocument::open(Reader(argv[i]));
-		if (!document)
+		const Reader reader{argv[i]};
+		if (!reader)
 		{
 			std::cerr << "ERROR: Unable to open source \"" << argv[i] << "\"\n";
 			return 1;
 		}
-		update_translation(*translation, document->root());
+		try
+		{
+			IonReader ion{reader};
+			enum { None, Open, Tr, Source, Close } stage = None;
+			StaticString source;
+			for (auto token = ion.read(); token.type() != IonReader::Token::Type::End; token = ion.read())
+			{
+				if (token.type() == IonReader::Token::Type::ObjectBegin)
+					stage = Open;
+				else if (stage == Open && token.type() == IonReader::Token::Type::Name && token.text() == "tr")
+					stage = Tr;
+				else if (stage == Tr && token.type() == IonReader::Token::Type::Value)
+				{
+					stage = Source;
+					source = token.text();
+				}
+				else if (stage == Source && token.type() == IonReader::Token::Type::ObjectEnd)
+					translation->add(source);
+				else
+					stage = None;
+			}
+		}
+		catch (const IonError& e)
+		{
+			std::cerr << "ERROR(" << argv[i] << "): " << e.what() << "\n";
+			return 1;
+		}
 	}
 
 	translation->remove_obsolete();
