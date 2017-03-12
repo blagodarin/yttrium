b2de61f604fd8fe5101c6e303057fb8179594804
Sergey Blagodarin <s.blagodarin@gmail.com>
2017-03-12 11:40:50 +0300

    Post-refactoring fix.

--- src/renderer/debug_renderer.cpp
+++ src/renderer/debug_renderer.cpp
@@ -45,7 +45,7 @@ namespace Yttrium
 
 	void DebugRenderer::draw_text(int x, int y, const StaticString& text, const boost::optional<size_t>& max_size)
 	{
-		const auto size = max_size ? text.size() : std::min(text.size(), *max_size);
+		const auto size = max_size ? std::min(text.size(), *max_size) : text.size();
 		for (size_t i = 0; i < size; ++i)
 		{
 			const auto symbol = static_cast<uint8_t>(text[i]);
