15a410d3cc7d4357bebf8466acdcf332f289263b
Sergey Blagodarin <s.blagodarin@gmail.com>
2017-05-01 00:02:49 +0300

    Printing time without Boost.

--- examples/utils.h
+++ examples/utils.h
@@ -3,12 +3,9 @@
 
 #include <yttrium/image.h>
 
-#include <array>
-#include <cstdio>
-#include <string>
+#include <iomanip>
+#include <sstream>
 #include <tuple>
-
-#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 inline auto make_bgra_tga(size_t width, size_t height, const std::function<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(size_t, size_t)>& callback)
 {
@@ -30,13 +27,10 @@ inline auto make_bgra_tga(size_t width, size_t height, const std::function<std::
 
 inline std::string make_screenshot_path()
 {
-	const auto date_time = boost::posix_time::second_clock::local_time();
-	const auto date = date_time.date();
-	const auto time = date_time.time_of_day();
-	std::array<char, 24> buffer;
-	std::snprintf(buffer.data(), buffer.size(), "%04d-%02d-%02d_%02d-%02d-%02d.png",
-		static_cast<int>(date.year()), static_cast<int>(date.month()), static_cast<int>(date.day()), time.hours(), time.minutes(), time.seconds());
-	return buffer.data();
+	const auto t = std::time(nullptr);
+	std::ostringstream s;
+	s << std::put_time(std::localtime(&t), "%Y-%m-%d_%H-%M-%S.png");
+	return s.str();
 }
 
 #endif
