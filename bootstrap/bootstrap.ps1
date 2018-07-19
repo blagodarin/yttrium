fddd0411c8d6a29e26ac45e151f487c84bece201
Sergey Blagodarin <s.blagodarin@gmail.com>
2018-07-20 00:53:09 +0300

    Update bootstrapping script

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -1,3 +1,12 @@
+Param(
+  [String]$Prefix,
+  [Switch]$All,
+  [Switch]$WithLibjpeg,
+  [Switch]$WithNasm,
+  [Switch]$WithOpenAL,
+  [Switch]$WithOpenGL
+)
+
 Import-Module BitsTransfer
 
 Function Y-Download([String] $url) {
@@ -19,31 +28,35 @@ Function Y-Extract([String] $archive, [String] $dir) {
 If (-Not (Test-Path -Path "build")) { New-Item -ItemType "directory" -Name "build" | Out-Null }
 Push-Location -Path "build"
 
-If (Test-Path -Path "prefix") { Remove-Item -Path "prefix" -Recurse -Force }
-New-Item -ItemType "directory" -Name "prefix" | Out-Null
-New-Item -ItemType "directory" -Name "prefix/include" | Out-Null
-New-Item -ItemType "directory" -Name "prefix/libs" | Out-Null
+New-Item -ItemType "directory" -Name "$($Prefix)/bin" -Force | Out-Null
+New-Item -ItemType "directory" -Name "$($Prefix)/include" -Force | Out-Null
+New-Item -ItemType "directory" -Name "$($Prefix)/libs" -Force | Out-Null
 
-# nasm
-Y-Download "http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip"
-Y-Extract "nasm-2.13.01-win64.zip" "nasm-2.13.01"
+If ($All -Or $WithNasm) {
+  Y-Download "http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip"
+  Y-Extract "nasm-2.13.01-win64.zip" "nasm-2.13.01"
+  Copy-Item -Path "nasm-2.13.01/nasm.exe" -Destination "$($Prefix)/bin"
+}
 
-# libjpeg-turbo
-Y-Download "https://downloads.sourceforge.net/project/libjpeg-turbo/1.5.3/libjpeg-turbo-1.5.3.tar.gz"
+If ($All -Or $WithLibjpeg) {
+  Y-Download "https://downloads.sourceforge.net/project/libjpeg-turbo/1.5.3/libjpeg-turbo-1.5.3.tar.gz"
+}
 
-# OpenAL
-Y-Download "http://openal-soft.org/openal-binaries/openal-soft-1.18.2-bin.zip"
-Y-Extract "openal-soft-1.18.2-bin.zip" "openal-soft-1.18.2-bin"
-Copy-Item -Path "openal-soft-1.18.2-bin/include" -Destination "prefix" -Recurse -Force
-Copy-Item -Path "openal-soft-1.18.2-bin/libs" -Destination "prefix" -Recurse -Force
-Y-Download "https://openal.org/downloads/oalinst.zip"
-Y-Extract "oalinst.zip" "oalinst.exe"
+If ($All -Or $WithOpenAL) {
+  Y-Download "http://openal-soft.org/openal-binaries/openal-soft-1.18.2-bin.zip"
+  Y-Extract "openal-soft-1.18.2-bin.zip" "openal-soft-1.18.2-bin"
+  Copy-Item -Path "openal-soft-1.18.2-bin/include" -Destination "$($Prefix)" -Recurse -Force
+  Copy-Item -Path "openal-soft-1.18.2-bin/libs" -Destination "$($Prefix)" -Recurse -Force
+  Y-Download "https://openal.org/downloads/oalinst.zip"
+  Y-Extract "oalinst.zip" "oalinst.exe"
+}
 
-# OpenGL
-New-Item -ItemType "directory" -Name "prefix/include/GL" | Out-Null
-Y-Download-To "https://khronos.org/registry/OpenGL/api/GL/glext.h" "prefix/include/GL"
-Y-Download-To "https://khronos.org/registry/OpenGL/api/GL/wglext.h" "prefix/include/GL"
-New-Item -ItemType "directory" -Name "prefix/include/KHR" | Out-Null
-Y-Download-To "https://khronos.org/registry/EGL/api/KHR/khrplatform.h" "prefix/include/KHR"
+If ($All -Or $WithOpenGL) {
+  New-Item -ItemType "directory" -Name "$($Prefix)/include/GL" | Out-Null
+  Y-Download-To "https://khronos.org/registry/OpenGL/api/GL/glext.h" "$($Prefix)/include/GL"
+  Y-Download-To "https://khronos.org/registry/OpenGL/api/GL/wglext.h" "$($Prefix)/include/GL"
+  New-Item -ItemType "directory" -Name "$($Prefix)/include/KHR" | Out-Null
+  Y-Download-To "https://khronos.org/registry/EGL/api/KHR/khrplatform.h" "$($Prefix)/include/KHR"
+}
 
 Pop-Location
