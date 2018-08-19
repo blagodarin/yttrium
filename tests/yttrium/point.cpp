d37dbe7709c5c28559fd4841d6d95c642c974285
Sergey Blagodarin <s.blagodarin@gmail.com>
2018-08-20 00:52:39 +0300

    Add stub for Catch2 tests

---
+++ tests/yttrium/point.cpp
@@ -0,0 +1,18 @@
+#include <yttrium/math/point.h>
+
+#include <catch/catch.hpp>
+
+TEST_CASE("Point tests")
+{
+	using Yttrium::Point;
+	{
+		Point p;
+		CHECK(p._x == 0);
+		CHECK(p._y == 0);
+	}
+	{
+		Point p{1, 2};
+		CHECK(p._x == 1);
+		CHECK(p._y == 2);
+	}
+}
