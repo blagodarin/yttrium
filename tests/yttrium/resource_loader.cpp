f922d7dbd836c8531caaa5059cca8d779655ef4f
Sergey Blagodarin <s.blagodarin@gmail.com>
2018-08-26 01:39:32 +0300

    Port more tests to Catch2

--- tests/yttrium/resource_loader.cpp
+++ tests/yttrium/resource_loader.cpp
@@ -6,7 +6,7 @@
 #include <yttrium/storage/storage.h>
 #include "test_utils.h"
 
-#include <boost/test/unit_test.hpp>
+#include <catch/catch.hpp>
 
 using Yttrium::DataError;
 using Yttrium::ResourceError;
@@ -16,7 +16,7 @@ using Yttrium::Translation;
 
 const std::string_view _translation_data = "tr \"foo\" \"bar\"";
 
-BOOST_AUTO_TEST_CASE(resource_loader_load)
+TEST_CASE("resource_loader.load")
 {
 	Storage storage{ Storage::UseFileSystem::Never };
 	storage.attach_buffer("one", ::make_buffer(_translation_data));
@@ -25,30 +25,30 @@ BOOST_AUTO_TEST_CASE(resource_loader_load)
 
 	ResourceLoader resource_loader{ storage };
 	const auto one = resource_loader.load_translation("one");
-	BOOST_CHECK(one);
-	BOOST_CHECK_EQUAL(one, resource_loader.load_translation("one"));
-	BOOST_CHECK_NE(one, resource_loader.load_translation("two"));
-	BOOST_CHECK_EQUAL(resource_loader.load_translation("two"), resource_loader.load_translation("two"));
-	BOOST_CHECK_THROW(resource_loader.load_translation("three"), ResourceError);
-	BOOST_CHECK_THROW(resource_loader.load_translation("bad"), DataError);
+	CHECK(one);
+	CHECK(one == resource_loader.load_translation("one"));
+	CHECK(one != resource_loader.load_translation("two"));
+	CHECK(resource_loader.load_translation("two") == resource_loader.load_translation("two"));
+	CHECK_THROWS_AS(resource_loader.load_translation("three"), ResourceError);
+	CHECK_THROWS_AS(resource_loader.load_translation("bad"), DataError);
 
-	BOOST_CHECK(!resource_loader.load_material("material"));
-	BOOST_CHECK(!resource_loader.load_mesh("mesh"));
-	BOOST_CHECK(!resource_loader.load_sound("sound"));
-	BOOST_CHECK(!resource_loader.load_texture_2d("texture_2d"));
+	CHECK(!resource_loader.load_material("material"));
+	CHECK(!resource_loader.load_mesh("mesh"));
+	CHECK(!resource_loader.load_sound("sound"));
+	CHECK(!resource_loader.load_texture_2d("texture_2d"));
 }
 
-BOOST_AUTO_TEST_CASE(resource_loader_open)
+TEST_CASE("resource_loader.open")
 {
 	Storage storage{ Storage::UseFileSystem::Never };
 	storage.attach_buffer("one", ::make_buffer(_translation_data));
 
 	ResourceLoader resource_loader{ storage };
-	BOOST_CHECK(resource_loader.open("one"));
-	BOOST_CHECK_THROW(resource_loader.open("two"), ResourceError);
+	CHECK(resource_loader.open("one"));
+	CHECK_THROWS_AS(resource_loader.open("two"), ResourceError);
 }
 
-BOOST_AUTO_TEST_CASE(resource_loader_release_unused)
+TEST_CASE("resource_loader.release_unused")
 {
 	Storage storage{ Storage::UseFileSystem::Never };
 	storage.attach_buffer("one", ::make_buffer(_translation_data));
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE(resource_loader_release_unused)
 	ResourceLoader resource_loader{ storage };
 	const auto one = resource_loader.load_translation("one");
 	const std::weak_ptr<const Translation> two = resource_loader.load_translation("two");
-	BOOST_CHECK(!two.expired());
+	CHECK(!two.expired());
 	resource_loader.release_unused();
-	BOOST_CHECK(two.expired());
+	CHECK(two.expired());
 }
---
+++ tests/yttrium/source.cpp
@@ -0,0 +1,89 @@
+#include <yttrium/memory/buffer.h>
+#include <yttrium/storage/source.h>
+#include <yttrium/storage/temporary_file.h>
+#include "iostream.h"
+#include "test_utils.h"
+
+#include <cstring>
+
+#include <catch/catch.hpp>
+
+using Yttrium::Buffer;
+using Yttrium::Source;
+
+TEST_CASE("source.from")
+{
+	CHECK(!Source::from("no_such_file"));
+	CHECK(Source::from(nullptr, 0));
+	CHECK(Source::from(Buffer{}));
+}
+
+TEST_CASE("source.from.source")
+{
+	const auto buffer = ::make_random_buffer(997);
+	const std::shared_ptr<Source> source = Source::from(buffer.data(), buffer.size());
+	{
+		const auto subsource = Source::from(source, 0, source->size());
+		REQUIRE(subsource->size() == source->size());
+		Buffer subbuffer(source->size());
+		CHECK(subsource->read_at(0, subbuffer.data(), subbuffer.size()) == subsource->size());
+		CHECK(!std::memcmp(subbuffer.begin(), buffer.begin(), subsource->size()));
+	}
+	{
+		const auto subsource = Source::from(source, 0, source->size() / 2);
+		REQUIRE(subsource->size() == source->size() / 2);
+		Buffer subbuffer(source->size());
+		CHECK(subsource->read_at(0, subbuffer.data(), subbuffer.size()) == subsource->size());
+		CHECK(!std::memcmp(subbuffer.begin(), buffer.begin(), subsource->size()));
+	}
+	{
+		const auto subsource = Source::from(source, source->size() / 2, source->size() - source->size() / 2);
+		REQUIRE(subsource->size() == source->size() - source->size() / 2);
+		Buffer subbuffer(source->size());
+		CHECK(subsource->read_at(0, subbuffer.data(), subbuffer.size()) == subsource->size());
+		CHECK(!std::memcmp(subbuffer.begin(), buffer.begin() + source->size() / 2, subsource->size()));
+	}
+}
+
+TEST_CASE("source.name")
+{
+	Yttrium::TemporaryFile tf;
+	const auto s = Source::from(tf.name());
+	REQUIRE(s);
+	CHECK(s->name() == tf.name());
+}
+
+TEST_CASE("source.property")
+{
+	const auto source = Source::from(nullptr, 0);
+	REQUIRE(source);
+	CHECK(source->property("name") == "");
+	source->set_property("name", "value");
+	CHECK(source->property("name") == "value");
+	source->set_property("name", "another value");
+	CHECK(source->property("name") == "another value");
+}
+
+TEST_CASE("source.size")
+{
+	CHECK(Source::from(Buffer{})->size() == 0);
+	CHECK(Source::from(Buffer{1})->size() == 1);
+	CHECK(Source::from(Buffer{997})->size() == 997);
+}
+
+TEST_CASE("source.to_buffer")
+{
+	const auto expected = ::make_random_buffer(997);
+	const auto actual = Source::from(expected.data(), expected.size())->to_buffer();
+	CHECK(expected == actual);
+	REQUIRE(actual.capacity() > actual.size());
+	CHECK(actual[actual.size()] == '\0');
+}
+
+TEST_CASE("source.to_string")
+{
+	const auto expected = ::make_random_buffer(997);
+	const auto actual = Source::from(expected.data(), expected.size())->to_string();
+	CHECK(actual.size() == expected.size());
+	CHECK(!std::memcmp(actual.data(), expected.data(), expected.size()));
+}
