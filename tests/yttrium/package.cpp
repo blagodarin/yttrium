5a41fa1a27a105463ee83e6066fdbd373383e9ef
Sergey Blagodarin <s.blagodarin@gmail.com>
2018-08-26 22:44:36 +0300

    Port more tests to Catch2

--- tests/yttrium/package.cpp
+++ tests/yttrium/package.cpp
@@ -1,12 +1,14 @@
+#include <yttrium/memory/buffer.h>
 #include <yttrium/storage/package.h>
 #include <yttrium/storage/source.h>
 #include <yttrium/storage/temporary_file.h>
 #include <yttrium/storage/writer.h>
 #include "iostream.h"
 
-#include <boost/test/unit_test.hpp>
-
 #include <array>
+#include <cstring>
+
+#include <catch2/catch.hpp>
 
 using namespace Yttrium;
 
@@ -19,7 +21,7 @@ namespace
 	}
 }
 
-BOOST_AUTO_TEST_CASE(package_)
+TEST_CASE("package")
 {
 	Buffer buffer1(100003);
 	::memset(buffer1.data(), 1, buffer1.size());
@@ -43,11 +45,11 @@ BOOST_AUTO_TEST_CASE(package_)
 
 	{
 		const auto package_writer = PackageWriter::create(package_file.name(), PackageType::Ypq);
-		BOOST_REQUIRE(package_writer);
-		BOOST_REQUIRE(package_writer->add(file1.name(), {}));
-		BOOST_REQUIRE(package_writer->add(file2.name(), {}));
-		BOOST_REQUIRE(package_writer->add(file3.name(), {}));
-		BOOST_REQUIRE(package_writer->commit());
+		REQUIRE(package_writer);
+		REQUIRE(package_writer->add(file1.name(), {}));
+		REQUIRE(package_writer->add(file2.name(), {}));
+		REQUIRE(package_writer->add(file3.name(), {}));
+		REQUIRE(package_writer->commit());
 	}
 
 	std::unique_ptr<Source> packed_file1;
@@ -56,28 +58,28 @@ BOOST_AUTO_TEST_CASE(package_)
 
 	{
 		const auto package = PackageReader::create(package_file.name(), PackageType::Ypq);
-		BOOST_REQUIRE(package);
+		REQUIRE(package);
 
 		packed_file3 = ::open_packed(*package, file3.name());
 		packed_file1 = ::open_packed(*package, file1.name());
 		packed_file2 = ::open_packed(*package, file2.name());
 	}
 
-	BOOST_REQUIRE(packed_file1);
-	BOOST_CHECK_EQUAL(packed_file1->to_buffer(), buffer1);
+	REQUIRE(packed_file1);
+	CHECK(packed_file1->to_buffer() == buffer1);
 
 	packed_file1 = {};
 
-	BOOST_REQUIRE(packed_file2);
-	BOOST_CHECK_EQUAL(packed_file2->to_buffer(), buffer2);
+	REQUIRE(packed_file2);
+	CHECK(packed_file2->to_buffer() == buffer2);
 
 	packed_file2 = {};
 
-	BOOST_REQUIRE(packed_file3);
-	BOOST_CHECK_EQUAL(packed_file3->to_buffer(), buffer3);
+	REQUIRE(packed_file3);
+	CHECK(packed_file3->to_buffer() == buffer3);
 }
 
-BOOST_AUTO_TEST_CASE(package_file_size)
+TEST_CASE("package.file_size")
 {
 	TemporaryFile file1;
 	Writer(file1).write_all(Buffer(1, "1"));
@@ -91,70 +93,70 @@ BOOST_AUTO_TEST_CASE(package_file_size)
 	TemporaryFile package_file;
 	{
 		const auto package_writer = PackageWriter::create(package_file.name(), PackageType::Ypq);
-		BOOST_REQUIRE(package_writer);
-		BOOST_REQUIRE(package_writer->add(file1.name(), {}));
-		BOOST_REQUIRE(package_writer->add(file2.name(), {}));
-		BOOST_REQUIRE(package_writer->add(file3.name(), {}));
-		BOOST_REQUIRE(package_writer->commit());
+		REQUIRE(package_writer);
+		REQUIRE(package_writer->add(file1.name(), {}));
+		REQUIRE(package_writer->add(file2.name(), {}));
+		REQUIRE(package_writer->add(file3.name(), {}));
+		REQUIRE(package_writer->commit());
 	}
 
 	const auto package = PackageReader::create(package_file.name(), PackageType::Ypq);
-	BOOST_REQUIRE(package);
+	REQUIRE(package);
 
 	auto packed_file = ::open_packed(*package, file2.name());
-	BOOST_REQUIRE(packed_file);
+	REQUIRE(packed_file);
 
 	std::array<uint8_t, 2> data{0, 0};
-	BOOST_CHECK_EQUAL(packed_file->read_at(0, data.data(), data.size()), 1);
-	BOOST_CHECK_EQUAL(data[0], '2');
+	CHECK(packed_file->read_at(0, data.data(), data.size()) == 1);
+	CHECK(data[0] == '2');
 }
 
-BOOST_AUTO_TEST_CASE(package_duplicates)
+TEST_CASE("package.duplicates")
 {
 	TemporaryFile package_file;
 	TemporaryFile file;
 	{
 		const auto package_writer = PackageWriter::create(package_file.name(), PackageType::Ypq);
-		BOOST_REQUIRE(package_writer);
+		REQUIRE(package_writer);
 		Writer(file).write_all(Buffer(1, "1"));
-		BOOST_REQUIRE(package_writer->add(file.name(), {}));
+		REQUIRE(package_writer->add(file.name(), {}));
 		Writer(file).write_all(Buffer(2, "23"));
-		BOOST_REQUIRE(package_writer->add(file.name(), {}));
-		BOOST_REQUIRE(package_writer->commit());
+		REQUIRE(package_writer->add(file.name(), {}));
+		REQUIRE(package_writer->commit());
 	}
 
 	const auto package = PackageReader::create(package_file.name(), PackageType::Ypq);
-	BOOST_REQUIRE(package);
+	REQUIRE(package);
 
 	auto packed_file = ::open_packed(*package, file.name());
-	BOOST_REQUIRE(packed_file);
+	REQUIRE(packed_file);
 
 	std::array<uint8_t, 3> data{0, 0, 0};
-	BOOST_REQUIRE_EQUAL(packed_file->read_at(0, data.data(), data.size()), 2);
-	BOOST_CHECK_EQUAL(data[0], '2');
-	BOOST_CHECK_EQUAL(data[1], '3');
+	REQUIRE(packed_file->read_at(0, data.data(), data.size()) == 2);
+	CHECK(data[0] == '2');
+	CHECK(data[1] == '3');
 }
 
-BOOST_AUTO_TEST_CASE(package_properties)
+TEST_CASE("package.properties")
 {
 	TemporaryFile package_file;
 	TemporaryFile file;
 	{
 		const auto package_writer = PackageWriter::create(package_file.name(), PackageType::Ypq);
-		BOOST_REQUIRE(package_writer);
-		BOOST_REQUIRE(package_writer->add(file.name(), {{"one", "two"}, {"three", "four"}}));
-		BOOST_REQUIRE(package_writer->commit());
+		REQUIRE(package_writer);
+		REQUIRE(package_writer->add(file.name(), {{"one", "two"}, {"three", "four"}}));
+		REQUIRE(package_writer->commit());
 	}
 
 	const auto package = PackageReader::create(package_file.name(), PackageType::Ypq);
-	BOOST_REQUIRE(package);
+	REQUIRE(package);
 
 	auto packed_file = ::open_packed(*package, file.name());
-	BOOST_REQUIRE(packed_file);
+	REQUIRE(packed_file);
 
-	BOOST_CHECK_EQUAL(packed_file->property("one"), "two");
-	BOOST_CHECK_EQUAL(packed_file->property("two"), "");
-	BOOST_CHECK_EQUAL(packed_file->property("three"), "four");
-	BOOST_CHECK_EQUAL(packed_file->property("four"), "");
-	BOOST_CHECK_EQUAL(packed_file->property("five"), "");
+	CHECK(packed_file->property("one") == "two");
+	CHECK(packed_file->property("two") == "");
+	CHECK(packed_file->property("three") == "four");
+	CHECK(packed_file->property("four") == "");
+	CHECK(packed_file->property("five") == "");
 }
