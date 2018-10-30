bc243d148f0ba7577e09a2dbe79b68a7d56ca33c
Sergey Blagodarin <s.blagodarin@gmail.com>
2018-10-30 23:53:44 +0300

    Fix GLSL forward compatibility issues

--- libs/yttrium/renderer/gl/2d_fs.glsl
+++ libs/yttrium/renderer/gl/2d_fs.glsl
@@ -5,7 +5,9 @@ in vec2 io_texcoord;
 
 uniform sampler2D surface_texture;
 
+out vec4 o_color;
+
 void main()
 {
-	gl_FragColor = io_color * texture2D(surface_texture, io_texcoord);
+	o_color = io_color * texture(surface_texture, io_texcoord);
 }
