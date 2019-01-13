91048dacaff6e5c66f512ff7f5e8f10ed54d25a4
Sergey Blagodarin <s.blagodarin@gmail.com>
2019-01-13 18:40:34 +0300

    Update copyright year

--- libs/core/storage/formats/ypq.h
+++ libs/core/storage/formats/ypq.h
@@ -1,5 +1,5 @@
 //
-// Copyright 2018 Sergei Blagodarin
+// Copyright 2019 Sergei Blagodarin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
 // limitations under the License.
 //
 
-#ifndef _src_storage_formats_ypq_h_
-#define _src_storage_formats_ypq_h_
+#pragma once
 
 #include <yttrium/memory/buffer.h>
 #include <yttrium/storage/package.h>
@@ -61,5 +60,3 @@ namespace Yttrium
 		bool _finished = false;
 	};
 }
-
-#endif
