92779fa9927f6de5b70e20d0be590d3e21549fe9
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-03-14 20:04:47 +0300

    Clean up CMake scripts

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,19 +1,6 @@
-#
 # This file is part of the Yttrium toolkit.
-# Copyright (C) 2019 Sergei Blagodarin.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright (C) Sergei Blagodarin.
+# SPDX-License-Identifier: Apache-2.0
 
 function(add_yglslc_sources _target)
 	if(NOT TARGET ${_target})
@@ -141,69 +128,4 @@ function(include_config _target _path)
 	else()
 		target_compile_options(${_target} PRIVATE -imacros ${_path})
 	endif()
-endfunction()
-
-function(_print_list)
-	cmake_parse_arguments(_arg "" "PREFIX" "VALUES" ${ARGN})
-	if(NOT "${_arg_VALUES}" STREQUAL "")
-		string(REPLACE ";" " " text "${_arg_VALUES}")
-	else()
-		set(text "(none)")
-	endif()
-	message(STATUS "${_arg_PREFIX}${text}")
-endfunction()
-
-function(_concatenate _result _first _second)
-	if("${${_first}}" STREQUAL "")
-		set(${_result} "${${_first}}${${_second}}" PARENT_SCOPE)
-	else()
-		set(${_result} "${${_first}} ${${_second}}" PARENT_SCOPE)
-	endif()
-endfunction()
-
-function(_print_config_flags _config)
-	message(STATUS "  ${_config}:")
-	string(TOUPPER ${_config} _CONFIG)
-	_concatenate(_flags CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_${_CONFIG})
-	_print_list(PREFIX "    CXX:                  " VALUES ${_flags})
-	if(WIN32)
-		_concatenate(_flags CMAKE_RC_FLAGS CMAKE_RC_FLAGS_${_CONFIG})
-		_print_list(PREFIX "    RC:                   " VALUES ${_flags})
-	endif()
-	_concatenate(_flags CMAKE_EXE_LINKER_FLAGS CMAKE_EXE_LINKER_FLAGS_${_CONFIG})
-	_print_list(PREFIX "    EXE_LINKER:           " VALUES ${_flags})
-	_concatenate(_flags CMAKE_MODULE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS_${_CONFIG})
-	_print_list(PREFIX "    MODULE_LINKER:        " VALUES ${_flags})
-	_concatenate(_flags CMAKE_SHARED_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS_${_CONFIG})
-	_print_list(PREFIX "    SHARED_LINKER:        " VALUES ${_flags})
-	_concatenate(_flags CMAKE_STATIC_LINKER_FLAGS CMAKE_STATIC_LINKER_FLAGS_${_CONFIG})
-	_print_list(PREFIX "    STATIC_LINKER:        " VALUES ${_flags})
-endfunction()
-
-function(print_current_flags _rc_flags _static_flags _dynamic_flags)
-	message(STATUS "Build flags:")
-	_print_config_flags("Debug")
-	_print_config_flags("MinSizeRel")
-	_print_config_flags("Release")
-	_print_config_flags("RelWithDebInfo")
-	message(STATUS "  (project):")
-	get_property(_compile_options DIRECTORY PROPERTY COMPILE_OPTIONS)
-	_print_list(PREFIX "    COMPILE_OPTIONS:      " VALUES ${_compile_options})
-	get_property(_compile_definitions DIRECTORY PROPERTY COMPILE_DEFINITIONS)
-	_print_list(PREFIX "    COMPILE_DEFINITIONS:  " VALUES ${_compile_definitions})
-	if(WIN32)
-		_print_list(PREFIX "    RC:                   " VALUES ${${_rc_flags}})
-	endif()
-	_print_list(PREFIX "    STATIC_LINKER:        " VALUES ${${_static_flags}})
-	_print_list(PREFIX "    (dynamic)_LINKER:     " VALUES ${${_dynamic_flags}})
-endfunction()
-
-function(y_sources _target _group)
-	if(NOT TARGET ${_target})
-		message(SEND_ERROR "'${_target}' is not a target")
-		return()
-	endif()
-	target_sources(${_target} PRIVATE ${ARGN})
-	string(REPLACE "/" "\\" _group "${_group}")
-	source_group("${_group}" FILES ${ARGN})
 endfunction()
